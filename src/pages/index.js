d5ecc1a4862512d1f59994378b6b52cae853344c
Tim L <tim@blueoceanideas.com>
2021-04-11 14:48:40 -0400

    responsive improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,15 @@ const links = [
 const IndexPage = () => {
   return (
     <main className="flex flex-col items-center justify-center text-gray-1 font-display">
-      <header className={"flex flex-start items-center w-full bg-primary-1 px-12 py-5 border-b-4 border-primary-2"}>
-        <div className="flex items-center flex-start" style={{maxWidth: "1440px"}}>
-          <img src={TimLongSquareSmall} className={"border-primary-2 rounded-full bg-green-100 border-2 mx-5"} alt="tim long" />
+      <header className={"flex justify-center md:justify-start items-center w-full bg-primary-1 px-12 py-5 border-b-4 border-primary-2"}>
+        <div className="flex items-center justify-start" style={{maxWidth: "1440px"}}>
+          <img src={TimLongSquareSmall} className={"border-primary-2 rounded-full bg-green-100 border-2 mr-5 md:mx-5"} alt="tim long" />
           <h1 className={"hidden"}>Tim Long (timuism)'s Virtual Home</h1>
-          <h2 className="text-5xl text-secondary-1 font-logo" style={{textShadow: "2px 2px 0px rgba(0, 0, 0, 0.25)"}}>timuism</h2>
+          <h2 className="text-3xl md:text-5xl text-secondary-1 font-logo" style={{textShadow: "2px 2px 0px rgba(0, 0, 0, 0.25)"}}>timuism</h2>
         </div>
       </header>
 
-      <div className="flex flex-col items-center justify-center max-w-3xl p-12">
+      <div className="flex flex-col items-center justify-center max-w-3xl">
 
         <Card>
           <div className="space-y-5">
@@ -39,14 +39,14 @@ const IndexPage = () => {
             </p>
           </div>
 
-          <nav className="flex flex-col justify-start w-full pt-8 space-y-2 md:space-y-0 md:space-x-8 md:flex-row">
+          <nav className="flex flex-col justify-start pt-8 space-y-2 md:space-y-0 md:space-x-8 md:flex-row">
             {links.map(link => <Link href={link.href} text={link.text} classList={navLinkClassList} />)}
           </nav>
         </Card>
 
-        <section className="flex flex-col items-center justify-center w-full p-12 m-5">
+        <section className="flex flex-col items-center justify-center p-12 m-5">
           <h5 className="pb-1 mb-5 uppercase border-b border-dotted opacity-75 border-gray-1">What's New</h5>
-          <p class="text-xl opacity-50">No Projects, Articles, or Links at this time 😭</p>
+          <p class="text-xl opacity-50">No Projects, Articles, or Links at this time <span role="img">😭</span></p>
         </section>
 
       </div>
