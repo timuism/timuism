6698db8ec136f1ac64fb55df18d18cd116b476f0
Tim L <tim@blueoceanideas.com>
2021-06-07 19:34:14 -0400

    move colors to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,25 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: {
-        1: '#6c05d3',
-        2: '#2e005b',
-      },
-      secondary: {
-        1: '#ccff00',
-      },
-      gray: {
-        1: '#585858',
-        2: '#474747'
-      }
-    },
     fontFamily: {
       'logo': ["'Vampiro One'", 'Helvetica', 'Arial', 'sans-serif'],
       'display': ["'Courier Prime'"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          1: '#6c05d3',
+          2: '#2e005b',
+        },
+        secondary: {
+          1: '#ccff00',
+        },
+        gray: {
+          1: '#585858',
+          2: '#474747'
+        }
+      },
+    },
   },
   variants: {
     extend: {},
