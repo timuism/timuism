d34a2c1cb3fc235950329dd5ebc9f28e602c8bba
Tim L <tim@blueoceanideas.com>
2021-07-22 13:18:13 -0400

    Make `PageTemplate` and new `WorkSamples` page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import * as React from "react";
 import "../styles/global.css";
+import PageTemplate from '../components/templates/page';
 import Card from '../components/card';
 import Link from '../components/link';
-import TimLongSquareSmall from "../images/TimLongSquareSmall.jpg";
-import TimLongWebDeveloperResume from "../files/TimLongWebDeveloperResume.pdf";
 
 const navLinkClassList = "text-primary-1 text-xl font-semibold no-underline hover:text-primary-2 hover:underline";
 const links = [
@@ -15,22 +14,7 @@ const links = [
 
 const IndexPage = () => {
   return (
-    <main className="flex flex-col items-center justify-center text-gray-1 font-display">
-      
-      <div id="banner" class="bg-green-500 text-white p-3 w-full text-center">
-        <p>Looking for my <strong>resume</strong>? <a href={TimLongWebDeveloperResume} class="underline hover:opacity-80 transition duration-300">You can download it here!</a></p>
-      </div>
-
-      <header className={"flex justify-center md:justify-start items-center w-full bg-primary-1 px-12 py-5 border-b-4 border-primary-2"}>
-        <div className="flex items-center justify-start" style={{maxWidth: "1440px"}}>
-          <img src={TimLongSquareSmall} className={"border-primary-2 rounded-full bg-green-100 border-2 mr-5 md:mx-5"} alt="tim long" />
-          <h1 className={"hidden"}>Tim Long (timuism)'s Virtual Home</h1>
-          <h2 className="text-3xl md:text-5xl text-secondary-1 font-logo" style={{textShadow: "2px 2px 0px rgba(0, 0, 0, 0.25)"}}>timuism</h2>
-        </div>
-      </header>
-
-      <div className="flex flex-col items-start justify-center max-w-7xl md:flex-row md:p-12">
-
+    <PageTemplate>
         <Card>
           <div className="space-y-5">
             <p className={"text-2xl"}>
@@ -101,9 +85,7 @@ const IndexPage = () => {
             </ul>
           </section>
         </Card>
-
-      </div>
-    </main>
+    </PageTemplate>
   )
 }
 
---
+++ src/pages/work-samples.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+import PageTemplate from '../components/templates/page';
+import Card from "../components/card";
+import Link from "../components/link";
+import "../styles/global.css";
+
+
+const WorkSamplesPage = () => {
+
+    const LarutaSites = [
+        {href: 'https://macpa.org', text: 'Maryland Association of Certified Public Accounts'},
+        {href: 'https://blionline.org', text: 'Business Learning Institute'},
+        {href: 'https://msba.org', text: 'Maryland State Bar Association'},
+        {href: 'https://cocpa.org', text: 'Colorado Society of CPAs'},
+    ];
+    
+    return (
+        <PageTemplate>
+            <div>
+                <Card>
+                    <h1 class="text-2xl gray-800 font-semibold">Laruta</h1>
+                    <div className="py-2 space-y-2">
+                        <p>Laruta is a web application that I have been a part of developing during my time with Blue Ocean Ideas. While, there have been a handful of developers that have touched the application (including the front-end), a good amount of the front end development work on the below sites has been implemented or updated by me.</p>
+                        <p>
+                            A lot of the features are behing a member subscription and difficult to showcase, but I have below a few snippets of code from some features:
+                        </p>
+                        <p><strong>Technologies used:</strong> HTML, CSS, LESS, SCSS, Semantic UI, Bootstrap, Javascript, jQuery, SelectJs, Algolia + React Instant Search, Vanilla JS, Wordpress, Laravel, Woocommerce, Gravity Forms, Advanced Custom Fields </p>
+                    </div>
+                    <ul>
+                        {
+                            LarutaSites.map((link) => (
+                                <li>
+                                    <Link href={link.href} text={link.text} />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                
+                </Card>
+
+                <Card>
+                    <h1>Laruta Sites</h1>
+                    <ul>
+                    
+                    </ul>
+                </Card>
+            </div>
+        </PageTemplate>
+        
+    );
+};
+
+export default WorkSamplesPage;
