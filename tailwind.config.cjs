4383c1afb3e2c4d76d6dbfa91b0dd37218b3a38e
Tim Long <timuism@me.com>
2023-02-19 22:08:21 -0500

    remove comment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     colors: {
-      // ["dceed1","aac0aa","736372","a18276","7a918d"]
       'transparent': 'transparent',
       'white': '#efe5ee',
       'black': '#020202',
