66322db92b606746424a9b6641c7b5a93b2ed1ec
Tim Long <timuism@me.com>
2023-06-06 22:02:27 -0400

    update to astro 2.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  })]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+  experimental: {
+    assets: true,
+  },
 });
